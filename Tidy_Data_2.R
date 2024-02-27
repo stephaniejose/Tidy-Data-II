88ff2b85b9425f983a80b483c388dba1466f9040
stephaniejose <bt21994@qmul.ac.uk>
2024-02-27 23:51:33 +0000

    gsub was used in conjuection with rename_with. this was to replace the words which had "opis" with "opris". I was unable to put "Copris" and "Copis" because they had both capital letter and lowercase letters. '.col = contains('-') tells rename_with to apply fixocopris function only to those columns whose names contain the underscore character ('_')
    pivot_longer was used to have less columns i.e. make the dataframe longer. the function stated to move all the words which contained '_' to move to a column called 'Spp' and their values to be put into a column called 'Values'
    
    '~' is a compact way to define an anonymous function that will be applied to each selected column name
    
    using 'separate_wider_delim' i was able to separate the genus and the species names into two separate columns. I had named the column i wanted to be modified and i had split it into the two i wanted and also specified how they must be separated with '-'
    
    Mutate can be used to fix mistakes and also create new columns. in this case i had replaced the underscores in the dataframe to empty spaces. i had done this by using gsub.
    
    mydf %>% mutate("new column = do_some_stuff(old_column)) - this 'formula' is used to create a new column
    
    mydf %>% mutate("old_column = do_some_stuff(old_column) - this is used to correct mistakes

--- Tidy_Data_2.R
+++ Tidy_Data_2.R
@@ -62,13 +62,78 @@ beetles %>% rename(c(Copris_agnus=Copis_agnus,
 #rename() changes the names of individual variables using new_name = old_name 
 #rename_with() renames columns using a function
 
+fixcopris <- function(x) 
+{gsub("opis","opris",x)}
+#the first line defines a function named fixedcopris that takes a single argument x. The argument is expected to be a string
+#the first argument is gsub is the pattern to be searched for which is "opis" and the second argument is the replacement string "opris". The third argument is the input string x where the substitution will be applied
 
+beetles <- rename_with(beetles, fixcopris, .col = contains('_'))
+#we used the rename_with that is used with other function to incorporate our fixcopris function in order to change more than one column at a time
+#the first argument in the rename_with is the dataframe we want to modify which is "beetles". The second argument is "fixcopris: which replaces occurrences of "opis" with "opris". 
+#'.col = contains('-') tells rename_with to apply fixcopris function only to those columns whose names contain the underscore character ('_')
 
+#Pivot the data into a table with a species name and counts using pivot_longer
+beetles <- beetles %>% pivot_longer(cols = contains('_'), 
+                         names_to ='Spp', 
+                         values_to ='Counts')
 
+?pivot_longer
 
+#Pipe the 'select' and 'rename' commands together, then adds your pivots
+beetles <- beetles %>% 
+  select(!starts_with('X')) %>% 
+  rename_with(~ gsub("opis", "opris", .x), .cols = contains ('_'))%>%
+  pivot_longer(cols = contains('_'), names_to ='Spp', values_to = 'Counts')
+#to pipe them together it required a bit of modification to my previous code when i had used them separately
+#'~' is used to create a formula, which, when used in functions like 'rename_with' it represents a shorthand for writing anonymous functions. It is a compact way to define an anonymous function that will be applied to each selected column name
 
+beetles <- rename_with(beetles, tolower)
 
+#SEPARATE
+#(Extra) How would you calculate the total number of Copris genus beetles caught without separating the columns? How many are there?
 
+#In order to calculate the total number of Copris genus beetles it would be easier to split it into two columns. 'separate_wider_delim' should be used
+
+?separate_longer_delim #splits by a delimiter 
+#separate_longer_position() splits by a fixed width
+
+#separate_longer_delim(data, cols, delim,...)
+#separate_longer_position(data, cols, width, ..., keep_empty = FALSE)
+
+beetles <- beetles %>% separate_wider_delim("spp",
+                                 names = c('genus','species'), 
+                                 delim = "_")
+#here i named the column that i would like to separate into two columns and called the new columns 'genus' and 'species. i kept getting error messages in regards to delim but i had just made delim='_' which is the space in between the species and genus names in order to show the separation and therefore translate them into different columns
+#'delim = '_'' the function interprets this to mean that it should split each value in the specified column wherever it finds an underscore character, and then create new columns based on these splits
+
+#MUTATE
+#Mutate will make an new column with the output of a function that we apply to a column 
+#Mutate can make things easier to read and make it looks more presentable e.g. remove the '_'
+#it can be used with any function that takes a vector, and returns a vector of the same length 
+#can be used to fix typos, capitalisations and other inconsistencies using functions like gsub / toupper / tolower
+
+#Example - to create a new column 
+mydf %>% mutate("new_column" = do_some_stuff(old_column) )
+
+#Example - fix typos, capitalisations etc
+mydf %>% mutate("old_column" = do_some_stuff(old_column) )
+
+#this will replace the underscores in our dataframe to empty space
+beetles %>% mutate("spp" = gsub('_',' ',spp))
+#this replaces all the underscores in the column spp using gsub. it subs out the '_' with empty space to make it look nicer
+
+casesdf <- read.table("WMR2022_reported_cases_3.txt",
+                      sep="\t",
+                      header=T,
+                      na.strings=c("")) %>% 
+  fill(country) %>% 
+  pivot_longer(cols=c(3:14),
+               names_to="year",
+               values_to="cases") %>%
+  pivot_wider(names_from = method,
+              values_from = cases)
+
+?rename
 
 
 
